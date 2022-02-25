6e33740310e34987c4310ede7a865abc4832ddd9
Fan-s <58429616+Fan-s@users.noreply.github.com>
2022-02-25 12:31:07 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+requirements = []
+with open("requirements.txt") as freq:
+    for line in freq.readlines():
+        requirements.append(line.strip())
+
+setup(
+    name="judge_tools",
+    version="0.1.0",
+    author="fan-s",
+    description="Sentence fluency and relevance detection tool",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
